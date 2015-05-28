6eddf4957c32be15a9550a6ab6cf4baf1e4ca5bd
WuXian <wuxian@buybal.com>
2015-05-28 13:54:12 +0800

    密钥证书进行加密
    更新

--- ViewController.m
+++ ViewController.m
@@ -27,7 +27,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.view.backgroundColor = [UIColor whiteColor];
+    self.view.backgroundColor = [UIColor grayColor];
     
     
     
